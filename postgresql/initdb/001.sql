87a72c3516c7d204f3475b0b736990bb230d60de
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2023-03-15 18:16:53 +0300

    Add a recipe for PostgreSQL database

---
+++ postgresql/initdb/001.sql
@@ -0,0 +1,11 @@
+create table clients (
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	phone VARCHAR(50),
+	age INT,
+	gender VARCHAR(50),
+	balance INT,
+	country VARCHAR(50),
+	city VARCHAR(50)
+);
